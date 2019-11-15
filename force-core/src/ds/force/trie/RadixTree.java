2e29778afc40b748d7131774402cf0d89a95b381
knockinHeavenDoor <61239289@qq.com>
2019-11-16 01:33:23 +0800

    base function

--- force-core/src/ds/force/trie/RadixTree.java
+++ force-core/src/ds/force/trie/RadixTree.java
@@ -1,4 +1,18 @@
 package ds.force.trie;
 
 public class RadixTree implements Trie{
+    @Override
+    public boolean find(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean add(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(String s) {
+        return false;
+    }
 }
--- force-core/src/ds/force/trie/Trie.java
+++ force-core/src/ds/force/trie/Trie.java
@@ -1,4 +1,10 @@
 package ds.force.trie;
 
 public interface Trie {
+
+    boolean find(String s);
+
+    boolean add(String s);
+
+    boolean remove(String s);
 }
