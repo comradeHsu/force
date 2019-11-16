89723da57b260e678932731415f5f762be69a2a2
knockinHeavenDoor <61239289@qq.com>
2019-11-17 01:32:22 +0800

    some function

--- force-core/src/ds/force/BinaryIndexedTree.java
+++ force-core/src/ds/force/BinaryIndexedTree.java
@@ -1,4 +1,31 @@
 package ds.force;
 
 public class BinaryIndexedTree {
+
+    transient int[] dataTable;
+
+    transient int[] treeArray;
+
+    private int size;
+
+    int lowBit(int m){
+        return m & (-m);
+    }
+
+    public void set(int index, int value){
+        dataTable[index] += value;
+        while(index <= size){
+            treeArray[index] += value;
+            index += lowBit(index);
+        }
+    }
+
+    public int getSum(int m){
+        int ans = 0;
+        while(m > 0){
+            ans += treeArray[m];
+            m -= lowBit(m);
+        }
+        return ans;
+    }
 }
