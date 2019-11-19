87395219a1dbcacca92ff57ed8dc0d836e95c194
knockinHeavenDoor <61239289@qq.com>
2019-11-19 16:33:01 +0800

    PrefixTree's basic function is finished

--- force-core/src/ds/force/trie/PrefixTree.java
+++ force-core/src/ds/force/trie/PrefixTree.java
@@ -2,12 +2,25 @@ package ds.force.trie;
 
 import ds.force.primitive.CharObjectMap;
 
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Deque;
+
 public class PrefixTree implements Trie {
 
     private transient TrieNode root;
 
+    private static final int SINGLE = 1;
+
     public PrefixTree(){
         this.root = new TrieNode();
+    }
+
+    public PrefixTree(Collection<String> c){
+        this.root = new TrieNode();
+        for (String word : c) {
+            add(word);
+        }
     }
 
     private static class TrieNode {
@@ -16,7 +29,6 @@ public class PrefixTree implements Trie {
 
         private boolean isEnd;
 
-        private char val;
 
         TrieNode() {
             childs = new CharObjectMap<>();
@@ -30,11 +42,29 @@ public class PrefixTree implements Trie {
         TrieNode getTrieNode(char character){
             return childs.get(character);
         }
+
+        boolean isEnd(){
+            return isEnd;
+        }
+    }
+
+    public boolean isEmpty(){
+        return this.root.childs.isEmpty();
     }
 
     @Override
-    public boolean find(String s) {
-        return false;
+    public boolean find(String word) {
+        char[] chars = word.toCharArray();
+        TrieNode node = this.root;
+        for (int i = 0; i < chars.length; i++) {
+            char character = chars[i];
+            if (node.contains(character)){
+                node = node.getTrieNode(character);
+            } else {
+                return false;
+            }
+        }
+        return node.isEnd();
     }
 
     @Override
@@ -56,7 +86,43 @@ public class PrefixTree implements Trie {
     }
 
     @Override
-    public boolean remove(String s) {
-        return false;
+    public boolean remove(String word) {
+        char[] chars = word.toCharArray();
+        TrieNode node = this.root;
+        Deque<TrieNode> stack = new ArrayDeque<>();
+        for (int i = 0; i < chars.length; i++) {
+            char character = chars[i];
+            stack.push(node);
+            if (node.contains(character)){
+                node = node.getTrieNode(character);
+            } else {
+                return false;
+            }
+        }
+        if (!node.isEnd()) return false;
+        int index = chars.length - 1;
+        while (!stack.isEmpty()){
+            TrieNode parent = stack.pop();
+            parent.childs.remove(chars[index]);
+            if (parent.childs.size() > SINGLE || parent.isEnd()){
+                break;
+            }
+            --index;
+        }
+        return true;
+    }
+
+    public TrieNode search(String prefix){
+        char[] chars = prefix.toCharArray();
+        TrieNode node = this.root;
+        for (int i = 0; i < chars.length; i++) {
+            char character = chars[i];
+            if (node.contains(character)){
+                node = node.getTrieNode(character);
+            } else {
+                return null;
+            }
+        }
+        return node;
     }
 }
