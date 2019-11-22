7586da5185b4129a63f17f75c672782640719184
knockinHeavenDoor <61239289@qq.com>
2019-11-22 21:30:33 +0800

    add test

--- force-core/src/ds/force/trie/SuffixTree.java
+++ force-core/src/ds/force/trie/SuffixTree.java
@@ -1,8 +1,19 @@
 package ds.force.trie;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 public class SuffixTree implements Trie{
 
+    private transient Node root;
+
     public SuffixTree(String word){
+    }
+
+    public SuffixTree(){
+        this.root = new Node();
     }
 
     private static class Edge {
@@ -11,10 +22,47 @@ public class SuffixTree implements Trie{
 
         private int to;
 
+        private Node node;
+
+        Edge(int from, int to) {
+            this.from = from;
+            this.to = to;
+        }
+
     }
 
     private static class Node {
 
+        private List<Edge> edges;
+
+        private Node link;
+
+        Node() {
+            this.edges = new ArrayList<>();
+        }
+
+        Edge getEdge(int index){
+            return edges.get(index);
+        }
+
+        void addEdge(Edge edge){
+            this.edges.add(edge);
+        }
+    }
+
+    private static class ActivePoint {
+
+        private Node activeNode;
+
+        private Edge activeEdge;
+
+        private int activeLength;
+
+        ActivePoint(Node activeNode, Edge activeEdge, int activeLength){
+            this.activeNode = activeNode;
+            this.activeEdge = activeEdge;
+            this.activeLength = activeLength;
+        }
     }
 
     @Override
@@ -22,9 +70,131 @@ public class SuffixTree implements Trie{
         return false;
     }
 
+    public void addd(String word){
+        char[] chars = word.toCharArray();
+        int remainder = 0;
+        ActivePoint activePoint = new ActivePoint(root,null,0);
+        Deque<Edge> stack = new ArrayDeque<>();
+        for (int c = 0; c < chars.length; c++) {
+            stack.addAll(root.edges);
+            remainder++;
+            while(!stack.isEmpty()){
+                Edge edge = stack.pop();
+                if (edge.node != null){
+                    stack.addAll(edge.node.edges);
+                } else {
+                    edge.to++;
+                }
+            }
+            Node preCreate = null;
+            int rem = remainder;
+            for (int i = 0; i < rem; i++) {
+                if (activePoint.activeEdge == null){
+                    activePoint.activeEdge = getEdge(activePoint.activeNode,chars[c]);
+                }
+                Edge edge = activePoint.activeEdge;
+                if (edge == null){
+                    edge = new Edge(c,c);
+                    activePoint.activeNode.addEdge(edge);
+                    activePoint.activeLength = activePoint.activeLength == 0 ? 0 : activePoint.activeLength--;
+                    remainder--;
+                    continue;
+                }
+                if (chars[c] == chars[edge.from+activePoint.activeLength]) {
+                    activePoint.activeLength++;
+                    remainder++;
+                    continue;
+                }
+                edge.to = edge.from + activePoint.activeLength - 1;
+                edge.node = edge.node == null ? new Node() : edge.node ;
+                if (preCreate == null){
+                    preCreate = edge.node;
+                } else {
+                    preCreate.link = edge.node;
+                    preCreate = edge.node;
+                }
+                edge.node.addEdge(new Edge(edge.from + activePoint.activeLength,c));
+                edge.node.addEdge(new Edge(c,c));
+                activePoint.activeEdge = getEdge(activePoint.activeNode, chars[edge.from+i]);
+                activePoint.activeLength--;
+                if (activePoint.activeNode != root && activePoint.activeNode.link != null){
+                    activePoint.activeNode = activePoint.activeNode.link;
+                } else {
+                    activePoint.activeNode = root;
+                }
+                remainder--;
+            }
+
+        }
+    }
+
     @Override
     public boolean add(String word) {
+        char[] chars = word.toCharArray();
+        int edgeSize = this.root.edges.size();
+        int remainder = 0;
+        ActivePoint activePoint = new ActivePoint(root,null,0);
+        for (int c = 0; c < chars.length; c++) {
+            boolean isExist = false;
+            remainder++;
+            for (int i = 0; i < edgeSize; i++) {
+                Edge edge = this.root.getEdge(i);
+                if (chars[edge.from] == chars[c]){
+                    activePoint.activeEdge = activePoint.activeEdge == null ? edge : activePoint.activeEdge;
+                    activePoint.activeLength++;
+                    remainder++;
+                    isExist = true;
+                } else {
+                    if (edge.node == null) {
+                        edge.to++;
+                    } else {
+                        Edge child = getEdge(edge.node,chars[c]);
+                        if (child != null) {
+                            activePoint.activeNode = edge.node;
+                            activePoint.activeEdge = child;
+                            activePoint.activeLength = 1;
+                        }
+                    }
+                }
+            }
+            Node preCreate = null;
+            for (int i = 0; i < remainder;i++) {
+                Edge edge = activePoint.activeEdge;
+                if (edge == null){
+                    edge = new Edge(c,c);
+                    activePoint.activeNode.addEdge(edge);
+                    activePoint.activeLength--;
+                    continue;
+                }
+                if (chars[c] == chars[edge.from+activePoint.activeLength]) {
+                    activePoint.activeLength++;
+                    remainder++;
+                    continue;
+                }
+                edge.to = edge.from + activePoint.activeLength - 1;
+                edge.node = edge.node == null ? new Node() : edge.node ;
+                if (preCreate == null){
+                    preCreate = edge.node;
+                } else {
+                    preCreate.link = edge.node;
+                    preCreate = edge.node;
+                }
+                edge.node.addEdge(new Edge(edge.from + activePoint.activeLength,c));
+                edge.node.addEdge(new Edge(c,c));
+                activePoint.activeEdge = getEdge(activePoint.activeNode, chars[edge.from+i]);
+                activePoint.activeLength--;
+                remainder--;
+            }
+            if (!isExist){
+                Edge edge = new Edge(c,c);
+                this.root.edges.add(edge);
+            }
+        }
         return false;
+    }
+
+    private Edge getEdge(Node node, char c){
+        return null;
     }
 
     @Override
---
+++ force-tests/test/ds/force/trie/SuffixTreeTest.java
@@ -0,0 +1,11 @@
+package ds.force.trie;
+
+import junit.framework.TestCase;
+
+public class SuffixTreeTest extends TestCase {
+
+    public void testAdd(){
+        SuffixTree tree = new SuffixTree();
+        tree.addd("bananas");
+    }
+}
