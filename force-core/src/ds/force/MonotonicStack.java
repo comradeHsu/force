6bf053d52676a2c0bf66fb7873e414b19928daae
knockinHeavenDoor <61239289@qq.com>
2019-11-26 01:09:51 +0800

    stack interface

--- force-core/src/ds/force/MonotonicStack.java
+++ force-core/src/ds/force/MonotonicStack.java
@@ -1,4 +1,28 @@
 package ds.force;
 
-public class MonotonicStack {
+public class MonotonicStack<E extends Comparable> implements Stack<E> {
+
+    transient Object[] elements;
+
+    transient boolean isIncrease;
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public E push(E item) {
+        return null;
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
 }
---
+++ force-core/src/ds/force/Stack.java
@@ -0,0 +1,13 @@
+package ds.force;
+
+public interface Stack<E> {
+
+    boolean isEmpty();
+
+    E push(E item);
+
+    E pop();
+
+    E peek();
+
+}
