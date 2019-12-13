a8331e204f5ef1409ba5d13754da4a7b2adef620
knockinHeavenDoor <61239289@qq.com>
2019-12-13 21:17:56 +0800

    some btreemap functions finished

--- force-core/src/ds/force/BTreeMap.java
+++ force-core/src/ds/force/BTreeMap.java
@@ -1,9 +1,12 @@
 package ds.force;
 
+import ds.force.util.ArrayUtil;
+
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.ToIntBiFunction;
 
 public class BTreeMap<K,V> implements Map<K,V>{
     /**
@@ -30,22 +33,28 @@ public class BTreeMap<K,V> implements Map<K,V>{
     public BTreeMap(){
         this.comparator = null;
         this.degree = DEFAULT_DEGREE;
-        this.root = new BTreeNode<>(degree);
+        this.root = new BTreeNode<>(degree,null);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public boolean containsKey(Object key) {
-        return false;
+        BTreeNode<K,V> target;
+        K k = (K) key;
+        if (comparator != null)
+            target = getNode(k,comparator::compare);
+        else target = getNode(k,(k1, k2) -> ((Comparable<? super K>)k1).compareTo(k2));
+        return target != null;
     }
 
     @Override
@@ -54,65 +63,211 @@ public class BTreeMap<K,V> implements Map<K,V>{
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public V get(Object key) {
+        BTreeNode<K,V> target;
+        K k = (K) key;
+        if (comparator != null)
+            target = getNode(k,comparator::compare);
+        else target = getNode(k,(k1, k2) -> ((Comparable<? super K>)k1).compareTo(k2));
+        if (target != null) return getEntry(target,k).value;
         return null;
     }
 
-    @Override
-    public V put(K key, V value) {
-        BTreeNode node = this.root;
-        while(node != null){
-            int i = 0;
-            in:for (; i < node.keys.length; i++){
-                if (node.keys[i] == null) break in;
-//                if (key < node.keys[i].key) break in;
-            }
-            node = node.childes[i];
-        }
-        return null;
-    }
-
-    private V putUsingComparator(K key, V value){
+    private BTreeNode<K,V> getNode(K key,final ToIntBiFunction<K,K> compare){
         BTreeNode<K,V> node = this.root;
         while(node != null){
             int index = 0;
-            for (int i = 0; i < node.keys.length; i++) {
+            for (int i = index; i < node.keys.length; i++) {
                 if (node.keys[i] == null) break;
-                int cmp = comparator.compare(key, node.keys[i].key);
+                int cmp = compare.applyAsInt(key, node.keys[i].key);
                 if (cmp < 0) break;
                 else if (cmp > 0) index++;
+                else return node;
             }
             node = node.childes[index];
         }
         return null;
     }
 
-    final void split(BTreeNode<K,V> node){
-        BTreeNode<K,V> left = new BTreeNode<>(degree);
+    @Override
+    @SuppressWarnings("unchecked")
+    public V put(K key, V value) {
+       if (comparator != null)
+           return putUsingFunction(key, value, comparator::compare);
+       return putUsingFunction(key, value, (k, k2) -> ((Comparable<? super K>)k).compareTo(k2));
+    }
+
+    private V putUsingComparator(K key, V value){
+        BTreeNode<K,V> node = this.root, preNode = this.root;
+        int index = 0;
+        while(node != null){
+            index = 0;
+            if (node.keys[node.keys.length-1] != null){
+                int preIndex = split(node);
+                node = node.parent;
+                index = preIndex;
+            }
+            for (int i = 0; i < node.keys.length; i++) {
+                if (node.keys[i] == null) break;
+                int cmp = comparator.compare(key, node.keys[i].key);
+                if (cmp < 0) break;
+                else if (cmp > 0) index++;
+            }
+            preNode = node;
+            node = node.childes[index];
+        }
+        NodeEntry<K,V> newNode = new NodeEntry<>(key,value);
+        ArrayUtil.add(preNode.keys,index,newNode);
+        size++;
+        return null;
+    }
+
+    private V putUsingFunction(K key, V value, final ToIntBiFunction<K,K> compare){
+        BTreeNode<K,V> node = this.root, preNode = this.root;
+        int index = 0;
+        while(node != null){
+            index = 0;
+            if (node.keys[node.keys.length-1] != null){
+                int preIndex = split(node);
+                node = preNode;
+//                continue;
+                index = preIndex;
+            }
+            for (int i = index; i < node.keys.length; i++) {
+                if (node.keys[i] == null) break;
+                int cmp = compare.applyAsInt(key, node.keys[i].key);
+                if (cmp < 0) break;
+                else if (cmp > 0) index++;
+                else return node.keys[i].setValue(value);
+            }
+            preNode = node;
+            node = node.childes[index];
+        }
+        NodeEntry<K,V> newNode = new NodeEntry<>(key,value);
+        ArrayUtil.add(preNode.keys,index,newNode);
+        size++;
+        return null;
+    }
+
+    private V putComparable(K key, V value){
+        BTreeNode<K,V> node = this.root, preNode = this.root;
+        int index = 0;
+        Comparable<? super K> k = (Comparable<? super K>) key;
+        while(node != null){
+            index = 0;
+            if (node.keys[node.keys.length-1] != null){
+                int preIndex = split(node);
+                node = node.parent;
+                index = preIndex;
+            }
+            for (int i = 0; i < node.keys.length; i++) {
+                if (node.keys[i] == null) break;
+                int cmp = k.compareTo(node.keys[i].key);
+                if (cmp < 0) break;
+                else if (cmp > 0) index++;
+            }
+            preNode = node;
+            node = node.childes[index];
+        }
+        NodeEntry<K,V> newNode = new NodeEntry<>(key,value);
+        ArrayUtil.add(preNode.keys,index,newNode);
+        return null;
+    }
+
+    final int split(BTreeNode<K,V> node){
+        BTreeNode<K,V> left = new BTreeNode<>(degree,null);
         int keysHalf = node.keys.length >> 1;
         int childesHalf = node.childes.length >> 1;
         System.arraycopy(node.keys, 0, left.keys, 0,
                keysHalf);
         System.arraycopy(node.childes, 0, left.childes, 0,
                 childesHalf);
-        BTreeNode<K,V> right = new BTreeNode<>(degree);
+        BTreeNode<K,V> right = new BTreeNode<>(degree,null);
         System.arraycopy(node.keys, keysHalf+1, right.keys, 0,
                 keysHalf);
         System.arraycopy(node.childes, keysHalf+1, right.childes, 0,
                 childesHalf);
         NodeEntry<K,V> half = node.keys[keysHalf];
+        for (int i = 0; i < childesHalf; i++){
+            if (left.childes[i] != null) left.childes[i].parent = left;
+            if (right.childes[i] != null) right.childes[i].parent = right;
+        }
         if (node.parent == null){
             node.clear();
             node.keys[0] = half;
             node.childes[0] = left;
             node.childes[1] = right;
-            return;
+            left.parent = right.parent = node;
+            return 0;
         }
+        int insertIndex = keyPoint(node.parent,half.key);
+        ArrayUtil.add(node.parent.keys,insertIndex,half);
+        node.parent.childes[insertIndex] = null;
+        ArrayUtil.addAll(node.parent.childes,insertIndex,new BTreeNode[]{left,right});
+        left.parent = right.parent = node.parent;
+        return insertIndex;
+    }
 
+    final void merge(BTreeNode<K,V> node,int keyPoint){
+        NodeEntry<K,V> k = node.keys[keyPoint];
+        BTreeNode<K,V> left = node.childes[keyPoint];
+        BTreeNode<K,V> right = node.childes[keyPoint+1];
+        ArrayUtil.remove(node.keys,keyPoint);
+        System.arraycopy(right.keys, 0, left.keys, degree,
+                degree-1);
+        left.keys[degree-1] = k;
+        System.arraycopy(right.childes, 0, left.childes, degree,
+                degree);
+        ArrayUtil.remove(node.childes,keyPoint+1);
     }
 
     @Override
     public V remove(Object key) {
+        BTreeNode<K,V> target = null;
+        final ToIntBiFunction<K,K> compare = null;
+        K k = (K) key;
+        BTreeNode<K,V> node = this.root;
+        while(node != null){
+            int index = 0;
+            if (node != root && node.keys[degree-1] == null){
+                int nodePoint = nodePoint(node);
+                BTreeNode<K,V>[] childes = node.parent.childes;
+                if (childes[nodePoint-1].keys[degree-1] != null){
+
+                }
+                else if (childes[nodePoint+1].keys[degree-1] != null){
+
+                }
+                else {
+
+                }
+            }
+            for (int i = index; i < node.keys.length; i++) {
+                if (node.keys[i] == null) break;
+                int cmp = compare.applyAsInt(k, node.keys[i].key);
+                if (cmp < 0) break;
+                else if (cmp > 0) index++;
+                else break;
+            }
+            node = node.childes[index];
+        }
+        if (target != null){
+            int keyPoint = keyPoint(target,k);
+            if (target.isLeaf()){
+
+            } else {
+                if (target.childes[keyPoint].keys[degree-1] != null){
+
+                }
+                else if (target.childes[keyPoint+1].keys[degree-1] != null){
+
+                }
+                else {
+                    merge(target,keyPoint);
+                }
+            }
+        }
         return null;
     }
 
@@ -123,7 +278,8 @@ public class BTreeMap<K,V> implements Map<K,V>{
 
     @Override
     public void clear() {
-
+        this.root.clear();
+        this.size = 0;
     }
 
     @Override
@@ -141,23 +297,44 @@ public class BTreeMap<K,V> implements Map<K,V>{
         return null;
     }
 
+    int nodePoint(BTreeNode<K,V> node){
+        BTreeNode<K,V>[] childes = node.parent.childes;
+        for (int i = 0; i < childes.length; i++) {
+            if (childes[i] == node) return i;
+        }
+        return -1;
+    }
+
     int keyPoint(BTreeNode<K,V> node, K key){
+        if (comparator != null)
+            return keyPointUsingComparator(node,key);
+        return keyPointComparable(node,key);
+    }
+
+    private int keyPointComparable(BTreeNode<K,V> node, K key){
+        int index = 0;
+        @SuppressWarnings("unchecked")
+        Comparable<? super K> k = (Comparable<? super K>) key;
+        for (int i = 0; i < node.keys.length; i++) {
+            if (node.keys[i] == null) break;
+            int cmp = k.compareTo(node.keys[i].key);
+            if (cmp < 0) break;
+            else if (cmp > 0) index++;
+            else break;
+        }
+        return index;
+    }
+
+    private int keyPointUsingComparator(BTreeNode<K,V> node, K key){
         int index = 0;
         for (int i = 0; i < node.keys.length; i++) {
             if (node.keys[i] == null) break;
             int cmp = comparator.compare(key, node.keys[i].key);
             if (cmp < 0) break;
             else if (cmp > 0) index++;
+            else break;
         }
-        return 0;
-    }
-
-    private int keyPointComparable(){
-        return 0;
-    }
-
-    private int keyPointUsingComparator(){
-        return 0;
+        return index;
     }
 
     private static class NodeEntry<K,V> implements Map.Entry<K,V> {
@@ -165,6 +342,11 @@ public class BTreeMap<K,V> implements Map<K,V>{
         K key;
 
         V value;
+
+        NodeEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
 
         @Override
         public K getKey() {
@@ -184,6 +366,29 @@ public class BTreeMap<K,V> implements Map<K,V>{
         }
     }
 
+    private NodeEntry<K,V> getEntry(BTreeNode<K,V> node,K key){
+        if (comparator != null) return getEntryUsingComparator(node,key);
+        return getEntryComparable(node,key);
+    }
+
+    private NodeEntry<K,V> getEntryComparable(BTreeNode<K,V> node,K key){
+        @SuppressWarnings("unchecked")
+        Comparable<? super K> k = (Comparable<? super K>) key;
+        for (int i = 0; i < node.keys.length; i++) {
+            if (key == node.keys[i].key || k.compareTo(node.keys[i].key) == 0)
+                return node.keys[i];
+        }
+        return null;
+    }
+
+    private NodeEntry<K,V> getEntryUsingComparator(BTreeNode<K,V> node,K key){
+        for (int i = 0; i < node.keys.length; i++) {
+            if (key == node.keys[i].key || comparator.compare(node.keys[i].key,key) == 0)
+                return node.keys[i];
+        }
+        return null;
+    }
+
     private static class BTreeNode<K,V> {
 
         NodeEntry<K,V>[] keys;
@@ -192,11 +397,11 @@ public class BTreeMap<K,V> implements Map<K,V>{
 
         BTreeNode<K,V> parent;
 
-        int keySize;
-
-        BTreeNode(int degree) {
+        @SuppressWarnings("unchecked")
+        BTreeNode(int degree, BTreeNode<K,V> parent) {
             this.keys = new NodeEntry[(degree<<1) - 1];
             this.childes = new BTreeNode[degree<<1];
+            this.parent = parent;
         }
 
         void clear(){
@@ -207,5 +412,8 @@ public class BTreeMap<K,V> implements Map<K,V>{
             childes[keys.length] = null;
         }
 
+        final boolean isLeaf(){
+            return childes[0] == null;
+        }
     }
 }
---
+++ force-tests/test/ds/force/BTreeMapTest.java
@@ -0,0 +1,17 @@
+package ds.force;
+
+import junit.framework.TestCase;
+
+public class BTreeMapTest extends TestCase {
+
+    public void testPut(){
+        BTreeMap<Integer,Integer> map = new BTreeMap<>();
+        map.put(3,3);
+        map.put(2,2);
+        map.put(4,4);
+        map.put(5,5);
+        for (int i = 6; i < 20; i++) {
+            map.put(i,i);
+        }
+    }
+}
